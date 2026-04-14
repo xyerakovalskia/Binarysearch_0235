8b4e9bd9e4436af822d98c4f55d40c9aecb7106f
xyerakovalskia <xyera123987@gmail.com>
2026-04-14 11:13:53 +0700

    voidbublesortarray

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -32,3 +32,21 @@ void input()
          cin >> element[i];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
