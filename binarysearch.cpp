655453cc0b89ae8d67927a93172158b8b1ae090b
xyerakovalskia <xyera123987@gmail.com>
2026-04-14 11:16:40 +0700

    voidbinarysearchdo

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -67,3 +67,18 @@ void display()
     }
     cout << endl;
 }
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n====================================\n";
+        cout << "        Pencarian Binary Search      \n";
+        cout << "====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
