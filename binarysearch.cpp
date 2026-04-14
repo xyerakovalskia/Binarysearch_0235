b9ae83405e6f94b787f11088f6979fe4facee7c9
xyerakovalskia <xyera123987@gmail.com>
2026-04-14 11:15:03 +0700

    voiddisplay

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -50,3 +50,20 @@ void bubbleSortArray()
         pass++;
     } while (pass <= nPanjang - 1);
 }
+
+void display()
+{
+    cout << "\n==============================================\n";
+    cout << "  Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "==============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
