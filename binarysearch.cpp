fc927c0f81945ea16a2d0f1058915f1d784e906b
xyerakovalskia <xyera123987@gmail.com>
2026-04-14 11:12:57 +0700

    voidinputbagian2

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -21,3 +21,14 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
+
+    cout << "\n==============================\n";
+    cout << "      Masukkan Elemen Array    \n";
+    cout << "==============================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+         cin >> element[i];
+    }
+}
