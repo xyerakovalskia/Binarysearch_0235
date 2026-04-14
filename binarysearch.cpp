8352d216761bcc5598d4ec92692c947f4043b972
xyerakovalskia <xyera123987@gmail.com>
2026-04-14 11:19:02 +0700

    elementmid=x

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -82,3 +82,13 @@ void binarySearch()
 
         int low = 0;
         int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
