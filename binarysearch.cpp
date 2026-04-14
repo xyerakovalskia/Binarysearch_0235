9ff2af36ae1b6f3b678916bcf378545456bf3d26
xyerakovalskia <xyera123987@gmail.com>
2026-04-14 11:11:47 +0700

    voidinput

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,3 +4,20 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
